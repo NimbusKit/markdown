2ccc8d8dafa339e63bb7c52a34b3f6e5cab5e411
Jose Alcalá-Correa <jose.alcala@xing.com>
2014-09-18 16:27:26 +0200

    Added example project, still empty

---
+++ Example/Example/XNGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XNGAppDelegate.h
+//  Example
+//
+//  Created by Jose Alcalá-Correa on 18/09/14.
+//  Copyright (c) 2014 XING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XNGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Jose Alcalá-Correa on 18/09/14.
+//  Copyright (c) 2014 XING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XNGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XNGAppDelegate class]));
+    }
+}
